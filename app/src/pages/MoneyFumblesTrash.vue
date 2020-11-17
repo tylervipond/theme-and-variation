cbe4db39762d9b08b94b2dd516557a2b39b35dae
Tyler Vipond <tylervipond@MacBook-Pro.local>
2020-11-16 23:41:43 -0500

    added money fumbles trash

---
+++ app/src/pages/MoneyFumblesTrash.vue
@@ -0,0 +1,36 @@
+<template>
+  <GifPage :gif-urls="urls" />
+</template>
+<script>
+import GifPage from "./GifPage.vue";
+
+export default {
+  name: "HorsesQuicksandSleepingCartoons",
+  components: { GifPage },
+  data() {
+    return {
+      urls: [
+        "money-fumbles-trash/fumble01.gif",
+        "money-fumbles-trash/fumble02.gif",
+        "money-fumbles-trash/fumble03.gif",
+        "money-fumbles-trash/fumble04.gif",
+        "money-fumbles-trash/fumble05.gif",
+        "money-fumbles-trash/fumble06.gif",
+        "money-fumbles-trash/fumble07.gif",
+        "money-fumbles-trash/fumble08.gif",
+        "money-fumbles-trash/money01.gif",
+        "money-fumbles-trash/money02.gif",
+        "money-fumbles-trash/money03.gif",
+        "money-fumbles-trash/money04.gif",
+        "money-fumbles-trash/money05.gif",
+        "money-fumbles-trash/money06.gif",
+        "money-fumbles-trash/trash01.gif",
+        "money-fumbles-trash/trash02.gif",
+        "money-fumbles-trash/trash03.gif",
+        "money-fumbles-trash/trash04.gif",
+        "money-fumbles-trash/trash05.gif"
+      ]
+    };
+  }
+};
+</script>
--- app/src/router.js
+++ app/src/router.js
@@ -1,12 +1,14 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import MasksVampiresPuppets from './pages/MasksVampiresPuppets.vue'
 import HorsesQuicksandSleepingCartoons from './pages/HorsesQuicksandSleepingCartoons.vue'
+import MoneyFumblesTrash from './pages/MoneyFumblesTrash.vue';
 import Home from './pages/Home.vue';
 
 const routes = [
     { path: '/', component: Home, meta: { title: 'Gif based art experiments by Tyler Vipond' } },
     { path: '/masks-vampires-puppets', component: MasksVampiresPuppets, meta: { title: 'Masks, Vampires, Puppets' } },
     { path: '/horses-quicksand-sleeping-cartoons', component: HorsesQuicksandSleepingCartoons, meta: { title: 'Horses, Quicksand, Sleeping Cartoons' } },
+    { path: '/money-fumbles-trash', component: MoneyFumblesTrash, meta: { title: 'Money, Fumbles, Trash' } },
 ]
 
 export const router = createRouter({
